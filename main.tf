e8e1d2ae8dd9717cfd2fe6e7e933e74b879d9a97
Roger Berlind <roger@hashicorp.com>
2018-01-24 20:33:08 -0500

    added outputs

--- main.tf
+++ main.tf
@@ -6,10 +6,20 @@ data "aws_s3_bucket" "selected" {
   bucket = "tfe-roger-ptfe-data"
 }
 
+resource "aws_s3_bucket_object" "example" {
+  key                    = "myname"
+  bucket                 = "${data.aws_s3_bucket.selected.id"
+  content                = "Roger"
+}
+
 output "arn" {
   value = "${data.aws_s3_bucket.selected.arn}"
 }
 
 output "domain_name" {
   value = "${data.aws_s3_bucket.selected.bucket_domain_name}"
+}
+
+output "object_id" {
+  value = "${aws_s3_bucket_object.example.id}"
 }
